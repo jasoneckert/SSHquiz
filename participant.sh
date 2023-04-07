52fda9e4a5ad821c73022f04fda19d13162c65e3
Jason Eckert <40586970+jasoneckert@users.noreply.github.com>
2023-04-07 18:45:31 -0400

    Added space after prompt

--- participant.sh
+++ participant.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Prompt the user for handle, remove any special characters, convert to lowercase, append PID to guarantee uniqueness
-echo -e "Welcome Quiz Participant!\n\nEnter your handle (up to 7 letters, no spaces or special characters)-->\c"
+echo -e "Welcome Quiz Participant!\n\nEnter your handle (up to 7 letters, no spaces or special characters)-->\c "
 read ANSWER
 export HANDLE=$(echo $ANSWER | tr -dc '[:alnum:]\n\r' | tr '[:upper:]' '[:lower:]')$$
 echo "Thankyou $HANDLE, get ready to start the quiz!"
@@ -17,7 +17,7 @@ for QUESTION in $(seq 1 20); do
   FILENAME=""
   while true; do
     SECONDS=0
-    echo -e "Please enter your choice for QUESTION $QUESTION (enter 1, 2, 3, or 4 only!)-->\c"
+    echo -e "Please enter your choice for QUESTION $QUESTION (enter 1, 2, 3, or 4 only!)-->\c "
     read CHOICE
     export RESULT=$HANDLE-$SECONDS-$CHOICE
     export FILENAME="/quiz/$QUESTION/$RESULT"
