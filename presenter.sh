de2a18bce83d937cce5cc50ab02ca0ac85ae1ebf
Jason Eckert <40586970+jasoneckert@users.noreply.github.com>
2023-04-07 20:17:57 -0400

    Add files via upload

--- presenter.sh
+++ presenter.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 #Check for root access
-#if [ $UID -ne 0 ]; then
-#  echo -e "You must be root to run this script.\nSwitch to root (feel the power!) and try again."
-#  exit 255
-#fi
-#if ! which chpasswd; then
-#  echo -e "You must install the chpasswd command."
-#  exit 255
-#fi
-#if ! which shuf; then
-#  echo -e "You must install the shuf command."
-#  exit 255
-#fi
+if [ $UID -ne 0 ]; then
+  echo -e "You must be root to run this script.\nSwitch to root (feel the power!) and try again."
+  exit 255
+fi
 
+#Check for required programs installed
+if ! which chpasswd; then
+  echo -e "You must install the chpasswd command."
+  exit 255
+fi
+if ! which shuf; then
+  echo -e "You must install the shuf command."
+  exit 255
+fi
+
+#Display instructions
 clear
 echo -e "What is the host name or IP address of your Linux server?--> \c"
 read SERVER
 echo -e "\nPRESENTER INSTRUCTIONS (READ CAREFULLY):\nWhen you press Enter, the SSH username and password\nthat participants should use will be displayed.\n\nAt this point, share your terminal window in your presentation\nand advise users to SSH into the server using these credentials.\n\nWhen you are satisfied that everyone is connected, press Enter to\nstart the quiz and display the first question.\n\nNarrate the question and answers as needed. When you feel enough time has\npassed for participants to answer, press Enter to view the results.\n\nAfter viewing the results, press Enter to continue to the next question.\n\nREADY TO START? PRESS ENTER TO DISPLAY THE SSH CREDENTIALS"
-mkdir -m 1777 /quiz
 read DUMMY
+
+#Create /quiz directory, add quiz user, display credentials
+mkdir -m 1777 /quiz
 useradd -m -s /bin/participant.sh quiz$$
 PW=$(shuf -n 1 /usr/share/dict/words)
 echo "quiz$$:$PW" | chpasswd
